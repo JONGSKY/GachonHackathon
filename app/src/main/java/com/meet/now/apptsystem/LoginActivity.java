d02441874a455743b6d2c4c4a6b0d49b524b4613
Brad Lee <brad903@naver.com>
2018-08-10 16:01:34 +0900

    로그인 기능 구현
    - 아이디, 비밀번호 확인 후 로그인
    - 로그인 후 메인 화면으로 전환

--- app/src/main/java/com/meet/now/apptsystem/LoginActivity.java
+++ app/src/main/java/com/meet/now/apptsystem/LoginActivity.java
@@ -1,12 +1,23 @@
 package com.meet.now.apptsystem;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +32,60 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
             }
         });
+
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 String userID = idText.getText().toString();
+                 String userPassword = passwordText.getText().toString();
+
+                 Response.Listener<String> responseListener = new Response.Listener<String>() {
+                     @Override
+                     public void onResponse(String response) {
+                         try{
+                             JSONObject jsonResponse = new JSONObject(response);
+                             boolean success = jsonResponse.getBoolean("success");
+                             if(success){
+                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                 dialog = builder.setMessage("로그인에 성공하셨습니다.")
+                                         .setPositiveButton("확인", null)
+                                         .create();
+                                 dialog.show();
+
+                                 // 화면 전환
+                                 Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                 LoginActivity.this.startActivity(loginIntent);
+                                 finish();
+                             }else{
+                                 // 로그인 실패
+                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                 dialog = builder.setMessage("아이디와 패스워드를 다시 확인하세요.")
+                                         .setNegativeButton("다시 시도", null)
+                                         .create();
+                                 dialog.show();
+                             }
+                         }catch(Exception e){
+                             e.printStackTrace();
+                         }
+                     }
+                 };
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(dialog != null){
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 }
---
+++ app/src/main/java/com/meet/now/apptsystem/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.meet.now.apptsystem;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest{
+
+    final static private String URL = "http://brad903.cafe24.com/UserLogin.php";
+    private Map<String, String> parameters;
+
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);  // 해당 파라미터를 POST방식으로 전송
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+        parameters.put("userPassword", userPassword);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return parameters;
+    }
+
+}
