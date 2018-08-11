9772513af7ae9de32f2d14f0ca61e3fc4db1c7cf
Brad Lee <brad903@naver.com>
2018-08-11 20:01:13 +0900

    친구 추가 화면 전환 + 친구생성 대화상자 열기

--- app/src/main/java/com/meet/now/apptsystem/FriendlistActivity.java
+++ app/src/main/java/com/meet/now/apptsystem/FriendlistActivity.java
@@ -1,4 +1,60 @@
 package com.meet.now.apptsystem;
 
-public class FriendlistActivity {
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class FriendlistActivity extends AppCompatActivity {
+
+    Dialog addfriendDialog;
+    Button searchfriend, closefriend;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friendlist);
+
+        ImageButton addfriendButton = (ImageButton)findViewById(R.id.addfriendlistButton);
+        addfriendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customAddfriendDialog();
+            }
+        });
+    }
+
+    public void customAddfriendDialog(){
+        addfriendDialog = new Dialog(FriendlistActivity.this);
+        addfriendDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        addfriendDialog.setContentView(R.layout.dialog_addfriend);
+        addfriendDialog.setTitle("TestCustomDialog");
+
+        searchfriend = (Button)addfriendDialog.findViewById(R.id.searchfriendButton);
+        closefriend = (Button)addfriendDialog.findViewById(R.id.closefriendButton);
+
+        searchfriend.setEnabled(true);
+        closefriend.setEnabled(true);
+
+        searchfriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Text CustomDialog", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        closefriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addfriendDialog.cancel();
+            }
+        });
+
+        addfriendDialog.show();
+    }
 }
--- app/src/main/java/com/meet/now/apptsystem/LoginActivity.java
+++ app/src/main/java/com/meet/now/apptsystem/LoginActivity.java
@@ -40,8 +40,8 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 String userID = idText.getText().toString();
-                 String userPassword = passwordText.getText().toString();
+                 final String userID = idText.getText().toString();
+                 final String userPassword = passwordText.getText().toString();
 
                  Response.Listener<String> responseListener = new Response.Listener<String>() {
                      @Override
@@ -58,6 +58,7 @@ public class LoginActivity extends AppCompatActivity {
 
                                  // 화면 전환
                                  Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                 loginIntent.putExtra("userID", userID);
                                  LoginActivity.this.startActivity(loginIntent);
                                  finish();
                              }else{
