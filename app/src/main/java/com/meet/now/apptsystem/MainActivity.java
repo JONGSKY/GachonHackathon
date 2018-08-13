047a99f265bc0b128db966bf2fe495fdbc633262
DESKTOP-KMB5M22\박민용 <pmy0237@naver.com>
2018-08-13 16:56:11 +0900

    앱 생성시 뷰페이저 추가

--- app/src/main/java/com/meet/now/apptsystem/MainActivity.java
+++ app/src/main/java/com/meet/now/apptsystem/MainActivity.java
@@ -1,13 +1,40 @@
 package com.meet.now.apptsystem;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.appt_create);
+
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+
+        pager.setOffscreenPageLimit(4);
+
+        appt_create_adapter adapter = new appt_create_adapter(getSupportFragmentManager());
+
+        appt_create_fragment1 fragment1 = new appt_create_fragment1();
+        adapter.additem(fragment1);
+
+        appt_create_fragment2 fragment2 = new appt_create_fragment2();
+        adapter.additem(fragment2);
+
+        appt_create_fragment3 fragment3 = new appt_create_fragment3();
+        adapter.additem(fragment3);
+
+        appt_create_fragment4 fragment4 = new appt_create_fragment4();
+        adapter.additem(fragment4);
+
+        pager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/meet/now/apptsystem/appt_create_fragment1.java
@@ -0,0 +1,19 @@
+package com.meet.now.apptsystem;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class appt_create_fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.appt_create_fragment1, container, false);
+        return rootView;
+    }
+}
